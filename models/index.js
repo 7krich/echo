317a2117f62514ad6a9150620475f0fd47d98a7a
Kyle Richnafsky <7krich@gmail.com>
2022-05-05 12:45:45 -0400

    added through for associations

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'product_id'
 })
 
 // Categories have many Products
@@ -16,6 +16,7 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
+  through: ProductTag,
   foreignKey: 'product_id'
 })
 
